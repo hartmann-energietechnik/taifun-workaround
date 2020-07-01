80c231d40bf71261672ba993735acd5a945cd8a2
tsmr <code@tsmr.eu>
2020-07-01 10:57:13 +0200

    Informationen auslesen für .pvprj hinzugefügt

--- fileinfo/pvprj.ps1
+++ fileinfo/pvprj.ps1
@@ -1,6 +1,8 @@
 
 # .pvprj ist ein einfaches Zip-Archiv
 
+# .\pvprj.ps1 -sourceFile ".\test\full sim.pvprj" -saveFile "./test.json"
+
 # ----- Informationen ------
 # PV-Generatorenleistung: OecForeCast.json : PVLeistung (in kWp)
 # Investitionskosten: OecForeCast.json : Investitionen.Data.Summe (in €)
@@ -33,12 +35,28 @@
 #           <Value></Value>
 
 param (
-    [string] $sourceFile = "";
-    [string] $saveResults = "";
+    [string] $sourceFile = "",
+    [string] $saveFile = ""
 )
 
-if ($sourceFile -eq "") exit;
-if ($saveResults -eq "") exit;
+$results = [pscustomobject] @{
+    PVLeistung = ''
+    Investitionskosten = ''
+    Amortisationszeit = ''
+    Module = @()
+    Batteriesystem = [pscustomobject] @{
+        Firma = ""
+        Modell = ""
+    }
+    ElektroAuto = @()
+};
+
+if ($sourceFile -eq "") {
+    exit;
+}
+if ($saveFile -eq "") {
+    exit;
+}
 
 $rand = Get-Random
 $tempFolder =  "$env:Temp\TaifunFiles\PVSOL\$rand"
@@ -52,14 +70,96 @@ Remove-Item $sourceFile
 $ProjectXML = "$tempFolder\Project.xml";
 $OecForeCastJSON = "$tempFolder\OecForeCast.json";
 
-if ((Test-Path -Path $ProjectXML) -ne $true ) exit;
-if ((Test-Path -Path $OecForeCastJSON) -ne $true ) exit;
+if ((Test-Path -Path $ProjectXML) -ne $true ) {
+    exit;
+}
+if ((Test-Path -Path $OecForeCastJSON) -ne $true ) {
+    exit;
+}
 
-$xml = [xml] (Get-Content $ProjectXML)
+$OecForeCastJSONData = Get-Content $OecForeCastJSON | Out-String | ConvertFrom-Json
 
-$xml.PVData.Batteriesystem.Company.Value
+$results.PVLeistung = ([String] $OecForeCastJSONData.PVLeistung) + " kWp";
+$results.Investitionskosten = ([String] $OecForeCastJSONData.Investitionen.Data.Summe) + " €";
+$results.Amortisationszeit = ([String] [Math]::Round($OecForeCastJSONData.ResultsModel.Data.Amortisationszeit, 2)) + " Jahre";
 
 
+$ProjectXMLData = [xml] (Get-Content $ProjectXML)
+
+$results.Batteriesystem.Firma = [String] $ProjectXMLData.PVData.Batteriesystem.Company.Value;
+$results.Batteriesystem.Modell = [String] $ProjectXMLData.PVData.Batteriesystem.SystemName.Value;
+
+$ParametersNode = $ProjectXMLData.PVData.PVModule.SelectNodes('Modulfelder')
+
+foreach($Node in $ParametersNode){
+    $results.Module += [pscustomobject] @{
+        Dach = [String] $Node.Modulname
+        Hersteller = [String] $Node.ModulHersteller
+        Name = [String] $Node.ModulName
+        Anzahl = [String]  $Node.Modulanzahl.Value
+    }
+}
+
+$ParametersNode = $ProjectXMLData.PVData.ElektroAuto.SelectNodes('Car')
+
+foreach($Node in $ParametersNode){
+    $results.ElektroAuto += [pscustomobject] @{
+        Hersteller = [String] $Node.ECarAndStationDBHersteller
+        Modell = [String] $Node.ECarAndStationDBName
+        Anzahl = [String] $Node.AnzahlAutos.Value
+        Reichweite = [String] $Node.GewuenschteReichweite.Value + " km"
+    }
+}
+
+function Format-Json {
+
+    # https://stackoverflow.com/questions/56322993/proper-formating-of-json-using-powershell/56324939
+
+    [CmdletBinding(DefaultParameterSetName = 'Prettify')]
+    Param(
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [string]$Json,
+
+        [Parameter(ParameterSetName = 'Minify')]
+        [switch]$Minify,
+
+        [Parameter(ParameterSetName = 'Prettify')]
+        [switch]$AsArray
+    )
+
+    [int]$Indentation = 2;
+
+    if ($PSCmdlet.ParameterSetName -eq 'Minify') {
+        return ($Json | ConvertFrom-Json) | ConvertTo-Json -Depth 100 -Compress
+    }
+
+    if ($Json -notmatch '\r?\n') {
+        $Json = ($Json | ConvertFrom-Json) | ConvertTo-Json -Depth 100
+    }
+
+    $indent = 0
+    $regexUnlessQuoted = '(?=([^"]*"[^"]*")*[^"]*$)'
+
+    $result = $Json -split '\r?\n' |
+        ForEach-Object {
+            if ($_ -match "[}\]]$regexUnlessQuoted") {
+                $indent = [Math]::Max($indent - $Indentation, 0)
+            }
+
+            $line = (' ' * $indent) + ($_.TrimStart() -replace ":\s+$regexUnlessQuoted", ': ')
+            if ($_ -match "[\{\[]$regexUnlessQuoted") {
+                $indent += $Indentation
+            }
+
+            $line
+        }
+
+    if ($AsArray) { return $result }
+    return $result -Join [Environment]::NewLine
+}
+
+Set-Content -Path $saveFile -Value ($results | convertto-json | Format-Json)
 
 # Aufräumen
+
 Remove-Item $tempFolder -Recurse
